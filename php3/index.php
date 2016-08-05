eead3faf9f37b31d9b94fd9b2a132b599395ea13
icomp <icomp@icomp-pc>
2016-08-05 17:47:01 -0400

    Incluído php3

---
+++ php3/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Login basicão</title>
+</head>
+<body>
+	<form name="formulario" onsubmit="enviar()">
+		Login:<input type="text" id="username"></input><br>
+		Senha:<input type="password" id="senha"></input><br>
+		<button type="submit">Conectar</button>
+	</form>
+</body>
+</html>
